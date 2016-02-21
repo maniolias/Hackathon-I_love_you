8b839ce69d12471b62075fbb5f2aff26bee9d5b5
crumiano <rumiano.c@coditt.fr>
2016-02-21 18:34:24 +0100

    Continu implementation

--- objects/map/maptile.js
+++ objects/map/maptile.js
@@ -9,7 +9,6 @@
         Phaser.Sprite.call(this, game, x, y, type);
 
         this.inputEnabled = true;
-        this.events.onInputDown.add(this.rotate, this);
         this.anchor.set(0.5);
         this.scale.set(1 / (nb / 2));
         this.isEnd = end;
